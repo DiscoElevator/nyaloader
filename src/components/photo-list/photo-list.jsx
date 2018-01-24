729f14e2e2356e87a0765e257ec11758ea9cc2f3
Maxim Tumas <Maxim_Tumas@epam.com>
2018-01-24 14:32:44 +0300

    Change photo layout

--- src/components/photo-list/photo-list.jsx
+++ src/components/photo-list/photo-list.jsx
@@ -3,15 +3,32 @@ import {plainBlock} from '@redneckz/react-bem-helper';
 import {Photo} from 'components/photo';
 import styles from './photo-list.scss';
 
+const COLUMN_COUNT = 5;
+
 export const PhotoList = plainBlock('photo-list', {styles})(({className, photos, selectedPhotos, onPhotoClick}) => (
     <div className={className}>
         {photos && photos.map(photo => (
-            <Photo
+            <PhotoListItem
                 key={photo.id}
                 photo={photo}
                 selected={selectedPhotos.findIndex(item => item.id === photo.id) > -1}
-                onClick={onPhotoClick}
+                columns={calculateColumns(photo)}
+                onPhotoClick={onPhotoClick}
             />
         ))}
     </div>
 ));
+
+const PhotoListItem = PhotoList.element('item')(({className, columns, photo, selected, onPhotoClick}) => (
+    <div className={className} style={{width: `${100 / COLUMN_COUNT * columns}%`}}>
+        <Photo
+            key={photo.id}
+            photo={photo}
+            selected={selected}
+            onClick={onPhotoClick}
+            columns={calculateColumns(photo)}
+        />
+    </div>
+));
+
+const calculateColumns = ({width, height}) => width > height ? 2 : 1;
--- src/components/photo/photo.jsx
+++ src/components/photo/photo.jsx
@@ -4,6 +4,10 @@ import styles from './photo.scss';
 
 export const Photo = plainBlock('photo', ({selected}) => ({selected}), {styles})(({className, photo, onClick}) => (
     <div className={className} onClick={() => onClick(photo)}>
-        <img src={photo.photo_130} alt="qqq" />
+        <Image src={photo.photo_604} title={photo.id} />
     </div>
+));
+
+const Image = Photo.element('image')(({className, title, src}) => (
+    <img className={className} src={src} alt={title} />
 ));
